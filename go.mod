bab7134ab69383514c0f48a9d6a35f153e1f2a70
flazepe <51487578+flazepe@users.noreply.github.com>
2022-03-26 00:48:18 +0800

    feat: initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/flazepe/osu_cleaner
+
+go 1.18
+
+require github.com/pborman/getopt/v2 v2.1.0
---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/pborman/getopt/v2"
+)
+
+func main() {
+	getopt.StringLong("dir", 'd', "", "the osu! directory")
+	getopt.Parse()
+
+	dir := getopt.GetValue("dir")
+
+	if dir == "" {
+		getopt.Usage()
+		os.Exit(1)
+	}
+
+	beatmaps, err := ioutil.ReadDir(dir)
+
+	if err != nil {
+		fmt.Printf(`Could not open the directory "%s".`, dir)
+		os.Exit(1)
+	}
+
+	var savedSpace int64
+
+	for _, beatmap := range beatmaps {
+		if !beatmap.IsDir() {
+			continue
+		}
+
+		beatmapDir := filepath.Join(dir, beatmap.Name())
+		files, err := ioutil.ReadDir(beatmapDir)
+
+		if err != nil {
+			panic(err)
+		}
+
+		importantFiles := map[string]bool{}
+
+		// Find important files
+		for _, file := range files {
+			if file.IsDir() || !strings.HasSuffix(file.Name(), ".osu") {
+				continue
+			}
+
+			osuContent, err := ioutil.ReadFile(filepath.Join(beatmapDir, file.Name()))
+
+			if err != nil {
+				panic(err)
+			}
+
+			for _, line := range strings.Split(string(osuContent), "\n") {
+				line = strings.TrimSpace(line)
+
+				if strings.HasPrefix(line, "AudioFilename: ") {
+					importantFiles[line[15:]] = true
+				} else if strings.HasPrefix(line, `0,0,"`) && strings.HasSuffix(line, `",0,0`) {
+					importantFiles[line[5:len(line)-5]] = true
+				}
+			}
+		}
+
+		// Remove all unimportant files and directories
+		for _, file := range files {
+			if strings.HasSuffix(file.Name(), ".osu") || importantFiles[file.Name()] {
+				continue
+			}
+
+			err := os.RemoveAll(filepath.Join(beatmapDir, file.Name()))
+
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println(filepath.Join(beatmap.Name(), file.Name()))
+			savedSpace += file.Size()
+		}
+	}
+
+	fmt.Printf("Saved %d MB!", savedSpace/1024/1024)
+}
